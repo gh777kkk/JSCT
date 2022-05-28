ea2395e2b271a658791703c0360e1e8542dc4df0
valueinvestment <78516146+valueinvestment@users.noreply.github.com>
2022-05-28 09:32:36 +0900

    Update 기타줄_1049.js

--- SeoungHoon(JS)/BaekJoon/기타줄_1049.js
+++ SeoungHoon(JS)/BaekJoon/기타줄_1049.js
@@ -13,7 +13,6 @@ const input = (() => {  //input()을 호출할 때마다 한줄씩 읽어온다.
 const [lineCount, brandCount] = input().split(' ').map(v => Number(v));
 const setCount = Math.ceil(lineCount / 6);
 const remainder = lineCount % 6;
-let result = 1000;
 let minSetPrice = 1000;
 let minEachPrice = 1000;
 
@@ -23,7 +22,7 @@ for(let i = 0; i < brandCount; i++){
     minEachPrice = Math.min(one, minEachPrice);
 }
 
-const combination = minSetPrice * (setCount -1) + (remainder * minEachPrice) ;
-result = Math.min(minSetPrice * setCount, minEachPrice * lineCount, combination);
+const combination = (minSetPrice * (setCount -1)) + (remainder * minEachPrice) ;
+const result = Math.min(minSetPrice * setCount, minEachPrice * lineCount, combination);
 
 console.log(result); // 결과 값 출력
